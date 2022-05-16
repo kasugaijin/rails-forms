537c606c18adb5a6a29107d6192eea1f29fafc09
Ben <robinsonbena@gmail.com>
2022-05-15 19:04:59 -0700

    HTML form name attribute changed to include User keys in brackets. Added privates params method to validate the hash being passed into the User#Create method. Tested and works - form submission creates new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # use the following instantiation if you don't use the key name inside brackets in the HTML form
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     
+    @user = User.new(user_params)
+
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  # because we cannot pass in a hash directly to #create for security reasons we pass in this private method
+  # that validates the hash data being passed in i.e., it requires the User hash with respective nested hash keys
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
