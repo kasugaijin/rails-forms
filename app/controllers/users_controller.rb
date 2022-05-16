0947722b4871b681e3c0cdd03ecfa9b51f27fb17
Ben <robinsonbena@gmail.com>
2022-05-16 12:05:33 -0700

    created user/edit.html.erb and made for using form helper, set form URL path and method, added validation error messaging, added User#Create method, tested and form works to update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   # because we cannot pass in a hash directly to #create for security reasons we pass in this private method
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users, only: [:new, :create] 
+  resources :users, only: [:new, :create, :edit, :update] 
 end
